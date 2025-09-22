ad55bc56b92553b609052ad4eb495b61ede7f37f
liamhardman1 <liamhardman.cs@gmail.com>
2025-09-22 01:44:21 -0500

    fix

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace ui_framework_program
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new main());
         }
     }
 }
